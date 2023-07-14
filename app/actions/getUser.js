24bd67a494e4af88193a1b656214f753f2723f69
thamer <thamer93work@gmail.com>
2023-07-14 16:58:23 +0300

    version 81

--- app/actions/getUser.js
+++ app/actions/getUser.js
@@ -3,7 +3,8 @@ import prisma from '../Libs/prismadb';
 export default async function UserMonth(){
     const userMonth = await prisma.user.findMany({
         
-        include:{months:true}
+        include:{months:true} ,
+        nocache: true,
     });
     if(!userMonth){return []}
 
--- app/home/page.jsx
+++
@@ -1,8 +0,0 @@
-import UserMonth from '../actions/getUser'
-
-export default async function Home(){
-    const allData = await UserMonth();
-    return(
-        <div>{JSON.stringify(allData)}</div>
-    )
-}
