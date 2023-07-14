79c12c52de0e6928b793e6ba6d52c13cb1eabe20
thamer <thamer93work@gmail.com>
2023-07-14 10:35:21 +0300

    version 11

--- app/(site)/page.jsx
+++ app/(site)/page.jsx
@@ -1,8 +1,9 @@
 import UsersList from '../components/UserList'
-import prisma from '../Libs/prismadb';
+import Users from '@/app/actions/getAllUsers'
 
 export default async function Home() {
-  const users = await prisma.user.findMany() || [];
+  const name = 'thamer'
+  const users = await Users(name) || [] ;
   
   return (
     <div className="p-5 ">
--- app/actions/getAllUsers.js
+++ app/actions/getAllUsers.js
@@ -1,6 +1,8 @@
 import prisma from '../Libs/prismadb';
 
- export default async function Users(){
+ export default async function Users({name}){
+    let one = name 
+    console.log(one)
     const users = await prisma.user.findMany();
 
     if(!users){return []}
