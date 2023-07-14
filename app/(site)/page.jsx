f6b16f327434265e0acad2b28a20f3b9a06a4034
thamer <thamer93work@gmail.com>
2023-07-14 09:57:06 +0300

    version 1.0.2

--- app/(site)/page.jsx
+++ app/(site)/page.jsx
@@ -1,9 +1,8 @@
 import Link from "next/link"
-import Users from '@/app/actions/getAllUsers'
+import prisma from '../Libs/prismadb';
 
 export default async function Home() {
-
-  const users = await Users();
+     const users = await prisma.user.findMany() || [];
   
   return (
     <div className="p-5 ">
